6128953ddfe7881f0330d5b38f36f296609da376
José de Assis <jassis@ibist.org.br>
2019-04-26 20:56:17 -0300

    Retirando bugs de dimencionamento no LoginComponent

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-login',
@@ -10,6 +11,31 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    $(function() {
+
+      $('.toggle-nav').click(function (e) {
+        e.stopPropagation();
+        toggleNav();
+      });
+
+      function toggleNav() {
+
+        // tslint:disable-next-line:no-unused-expression
+        var btnlock = $('.btn-lock>i');
+
+        if ($('#wrapper').hasClass('show-nav')) {
+          btnlock.removeClass('fa-unlock');
+          btnlock.addClass('fa-lock');
+          $('#wrapper').removeClass('show-nav');
+        } else {
+          btnlock.removeClass('fa-lock');
+          btnlock.addClass('fa-unlock');
+          $('#wrapper').addClass('show-nav');
+        }
+      }
+
+    });
   }
 
 }
