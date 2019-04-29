55744ca4ede2aa249c4c7c8ed7ab04fb4b66741b
José de Assis <jassis@ibist.org.br>
2019-04-29 19:21:05 -0300

    Inclusão do Login-form no LoginComponent

---
+++ src/app/login/login-form/login-form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.scss', '../login.component.scss']
+})
+export class LoginFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,9 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [
+    LoginComponent,
+    LoginFormComponent
+  ],
   imports: [
     CommonModule,
     LoginRoutingModule
